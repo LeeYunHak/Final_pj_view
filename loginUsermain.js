056fe015618762351f1a5bf6d8dd989ad1fdc73d
LYH <sza0203@naver.com>
2021-06-29 15:03:02 +0900

    0629 15:03

---
+++ loginUsermain.js
@@ -0,0 +1,51 @@
+document.getElementById('selectnavi').addEventListener('mouseover', function() {
+
+    document.getElementsByClassName('opennavi')[0].style.display ='block'
+});
+document.getElementsByClassName('opennavi')[0].addEventListener('mouseover', function() {
+
+    document.getElementsByClassName('opennavi')[0].style.display ='block'
+});
+document.getElementsByClassName('opennavi')[0].addEventListener('mouseout', function() {
+
+    document.getElementsByClassName('opennavi')[0].style.display ='none'
+});
+
+
+// document.getElementById('search1').addEventListener('click', function() {
+
+//     document.getElementsByClassName('arrow_box')[0].style.display ='block'
+// });
+// document.getElementsByClassName('arrow_box')[0].addEventListener('mouseover', function() {
+
+//     document.getElementsByClassName('arrow_box')[0].style.display ='block'
+// });
+// document.getElementsByClassName('arrow_box')[0].addEventListener('mouseout', function() {
+
+//     document.getElementsByClassName('arrow_box')[0].style.display ='none'
+// });
+
+
+document.getElementById('mypage1').addEventListener('click', function() {
+
+    document.getElementsByClassName('arrow_box')[0].style.display ='block'
+});
+document.getElementsByClassName('arrow_box')[0].addEventListener('mouseover', function() {
+
+    document.getElementsByClassName('arrow_box')[0].style.display ='block'
+});
+document.getElementsByClassName('arrow_box')[0].addEventListener('mouseout', function() {
+
+    document.getElementsByClassName('arrow_box')[0].style.display ='none'
+});
+
+// const input = document.getElementById("search-input");
+// const searchBtn = document.getElementById("search-btn");
+
+// const expand = () => {
+//   searchBtn.classList.toggle("close");
+//   input.classList.toggle("square");
+// };
+
+// searchBtn.addEventListener("click", expand);
+
