efea1ca868f910f640dcc43178a537326a998ac0
LYH <sza0203@naver.com>
2021-07-12 12:55:37 +0900

    파일 수정본 중간 종합 0712 12:55

--- CVpageJs.js
+++ CVpageJs.js
@@ -1,3 +1,31 @@
+/*네비바*/
+document.getElementById('nav3-2-Select').addEventListener('mouseover', function() {
+
+    document.getElementsByClassName('opennavi')[0].style.display ='block'
+  });
+  document.getElementsByClassName('opennavi')[0].addEventListener('mouseover', function() {
+  
+    document.getElementsByClassName('opennavi')[0].style.display ='block'
+  });
+  document.getElementsByClassName('opennavi')[0].addEventListener('mouseout', function() {
+  
+    document.getElementsByClassName('opennavi')[0].style.display ='none'
+  });
+  
+  document.getElementById('nav3-7-MyPage').addEventListener('click', function() {
+  
+    document.getElementsByClassName('arrow_box')[0].style.display ='block'
+  });
+  document.getElementsByClassName('arrow_box')[0].addEventListener('mouseover', function() {
+  
+    document.getElementsByClassName('arrow_box')[0].style.display ='block'
+  });
+  document.getElementsByClassName('arrow_box')[0].addEventListener('mouseout', function() {
+  
+    document.getElementsByClassName('arrow_box')[0].style.display ='none'
+  });
+
+
 var addBtn = document.getElementById("addBtn");
 addBtn.addEventListener('click',function(){
 
--- companypost_jq.js
+++ companypost_jq.js
@@ -1,33 +1,3 @@
-/*북마크 모달창 띄우기*/
-// function openModal(my_modal1){
-//     document.get
-//     $("#full").fadeIn(300);
-//     $("."+my_modal1).fadeIn(300);
-//   }  
-//   $("#full, .Modal_close_btn1").on('click',function(){
-//     $("#modal").fadeOut(300);
-//     $(".my_modal1").fadeOut(300);
-//   });
-
-// $(function(){ 
-//     $("#bookmarkbtn").click(function(){ 
-//         $(".doc").fadeIn();
-//     }); 
-//     $(".my_modal1").click(function(){
-//         $(".full").fadeOut(); 
-//     }); 
-// });
-
-// $().ready(function() {
-//     $("#modal_show").click(function() {
-//         $("#my_modal1").modal("show");
-//     });
-
-//     $("#close_modal").click(function() {
-//         $("#emy_modal1").modal("hide");
-//     });
-// });
-
 
 /*북마크클릭시 색변경 */
 $('#bm').on({
@@ -35,17 +5,3 @@ $('#bm').on({
         $('#bookmarkimg').attr('src','images/bookmark2.png'); 
     } 
 });
-
-// $(document).ready(function(){
-    //     /*img1을 클릭했을 때 img2를 보여줌*/
-    // $("#bm").click(function(){
-    //     $("#bookmarkimg2").hide();
-    //     $("#bookmarkimg1").show();
-    // });
-    
-    //     /*img2를 클릭했을 때 img1을 보여줌*/
-//     $("#img2").click(function(){
-//         $("#img1").show();
-//         $("#img2").hide();
-//     });
-// });
